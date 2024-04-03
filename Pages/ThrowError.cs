68ed9a7c6c664f9c341f07585a055940fdaee2b7
Kevin Chan <kechan@bchousing.org>
2024-04-03 09:30:16 -0700

    Attempt to add new page to generate proper error code

---
+++ Pages/ThrowError.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotnetcoresample.Pages;
+
+public class ThrowErrorModel : PageModel
+{
+    private readonly ILogger<ThrowErrorModel> _logger;
+
+    public ThrowErrorModel(ILogger<ThrowErrorModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        throw new Exception("Attempting to generate error page...");
+    }
+}
+
